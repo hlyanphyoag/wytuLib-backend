5bb1e6af67f7dacaa9140c7cbd17fc99e8573e68
hlyanphyo27 <hlyanphyozyx27122003@gmail.com>
2026-05-13 22:51:23 +0630

    Remove: prisma_accelerate

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { withAccelerate } from '@prisma/extension-accelerate';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -10,7 +9,7 @@ export class PrismaService implements OnModuleInit, OnModuleDestroy {
     private prisma;
 
     constructor() {
-        this.prisma = new PrismaClient({ accelerateUrl: process.env.DATABASE_URL }).$extends(withAccelerate());
+        this.prisma = new PrismaClient();
     }
 
     async onModuleInit() {
